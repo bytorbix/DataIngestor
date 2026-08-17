4a10e8ee933cd9f55ed02ae9e67450eb00f06190
bytor <bytorbix@gmail.com>
2026-08-17 15:25:44 +0300

    Implemented Kafka Consumer for data ingestor

---
+++ DataIngestor/KafkaBlock/KafkaBlock.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+using static Confluent.Kafka.ConfigPropertyNames;
+
+namespace DataIngestor.KafkaBlock
+{
+    
+    public class KafkaBlock
+    {
+        public string KAFKA_TOPIC_NAME = "telemetry";
+        public IConfiguration configuration;
+        private IConsumer<string, string> consumer;
+        private ILogger<KafkaBlock> _logger;
+
+        public KafkaBlock(IConfiguration configuration, ILogger<KafkaBlock> logger)
+        {
+            this.configuration = configuration;
+            this._logger = logger;
+
+            ConsumerConfig config = new()
+            {
+                BootstrapServers = configuration["Kafka:BootstrapServers"],
+                GroupId = configuration["Kafka:GroupId"],
+                AutoOffsetReset = AutoOffsetReset.Latest
+            };
+
+            consumer = new ConsumerBuilder<string, string>(config).Build();
+            consumer.Subscribe(KAFKA_TOPIC_NAME);
+
+            Task.Run(ConsumeLoop);
+        }
+
+        private void ConsumeLoop()
+        {
+            while (true)
+            {
+                ConsumeResult<string, string> result = consumer.Consume();
+                _logger.LogInformation("[{Key}] {Value}", result.Message.Key, result.Message.Value);
+            }
+        }
+
+
+    }
+}
